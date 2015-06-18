6bec87f7f0f21fcc45d1d095e18fba892b675afb
ezequielBrandao <ezequiel.brandao@gmail.com>
2015-06-17 23:32:58 -0400

    Revert "Alterações do trello."
    This reverts commit 7054c0269f013764270524a5ccb17802c1a1cd09.

--- assets/js/datas.js
+++ assets/js/datas.js
@@ -2,11 +2,15 @@ $(function(){
     if ($('input[name="bloquear_boleto"]').val()){ 
         $("#pagBoleto")[0].href="#";
         $("#pagBoleto").click(function(){
-            alert("Opção boleto desabilitada, pagamentos por boleto são desabilitados 3 dias antes da data final de agendamento.");
+            alert("Esta forma de pagamento está desabilitada!");
             $("#pagBoleto").href="null";
             setInterval(function(){ $("#pagCard").click(); }, 50);
         } );
-        $("#pagBoleto")[0].title="Opção boleto desabilitada, pagamentos por boleto são desabilitados 3 dias antes da data final de agendamento.";
+        $("#pagBoleto")[0].title="Opção boleto desabilitada, pagamentos por boleto somente com 3 dias de antecedência.";        
+    }
+    if ($('input[name="bloquear_cartao"]').val()){ 
+        $("#pagCard")[0].hidden=true;
+        $("#pagCard2")[0].hidden=false;
     }
 
 });
