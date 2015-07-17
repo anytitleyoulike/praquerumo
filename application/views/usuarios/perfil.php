d7ebe476bbb99662517aab66194fcd89bda5bd9f
Marcello <maarc.hen@gmail.com>
2015-07-17 18:47:46 -0400

    removendo divs desnecessárias

--- application/views/usuarios/perfil.php
+++ application/views/usuarios/perfil.php
@@ -257,16 +257,6 @@
 						  </div>
 						  <div class="col-md-4">
 						  </div>
-						  <div class="col-md-4 offset-0">
-							<table class="table table-bordered  mt-10">
-							<tr class="grey opensans">
-								<td class="center"><span class="size30 slim lh4">2</span><br/><span class="size12">Trips</span></td>
-								<td class="center"><span class="size30 slim lh4">5</span><br/><span class="size12">Places</span></td>
-								<td class="center"><span class="size30 slim lh4">14</span><br/><span class="size12">Days away</span></td>
-								<td class="center"><span class="size30 slim lh4">3</span><br/><span class="size12">Countries</span></td>
-							</tr>
-							</table>
-						  </div>
 						  <!-- End of Admin top -->
 						  
 						  
@@ -279,29 +269,7 @@
 
 						<!-- COL 1 -->
 						<div class="col-md-12 offset-0">
-							<table>
-								<tr>
-									<td>
-										<div class="radio left">
-										  <label>
-											<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-											Ms. 
-										  </label>
-										</div>
-									</td>
-									<td>
-										<div class="radio">
-										  <label>
-											<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-											Mr. 
-										  </label>
-										</div>
-									</td>
-								</tr>
-							</table>
 							
-							
-							<br/>
 							Name*:
 							<input type="text" class="form-control" placeholder="John Doe" rel="popover" id="name" data-content="This field is mandatory" data-original-title="Here you can edit your name">
 							<br/>
@@ -1192,89 +1160,6 @@
 		
 	</div>
 	<!-- END OF CONTENT -->
-	
-
-	
-	
-	<!-- FOOTER -->
-	
-	<div class="footerbgblack">
-		<div class="container">		
-			
-			<div class="col-md-3">
-				<span class="ftitleblack">Let's socialize</span>
-				<div class="scont">
-					<a href="#" class="social1b"><img src="images/icon-facebook.png" alt=""/></a>
-					<a href="#" class="social2b"><img src="images/icon-twitter.png" alt=""/></a>
-					<a href="#" class="social3b"><img src="images/icon-gplus.png" alt=""/></a>
-					<a href="#" class="social4b"><img src="images/icon-youtube.png" alt=""/></a>
-					<br/><br/><br/>
-					<a href="#"><img src="images/logosmal2.png" alt="" /></a><br/>
-					<span class="grey2">&copy; 2013  |  <a href="#">Privacy Policy</a><br/>
-					All Rights Reserved </span>
-					<br/><br/>
-					
-				</div>
-			</div>
-			<!-- End of column 1-->
-			
-			<div class="col-md-3">
-				<span class="ftitleblack">Travel Specialists</span>
-				<br/><br/>
-				<ul class="footerlistblack">
-					<li><a href="#">Golf Vacations</a></li>
-					<li><a href="#">Ski & Snowboarding</a></li>
-					<li><a href="#">Disney Parks Vacations</a></li>
-					<li><a href="#">Disneyland Vacations</a></li>
-					<li><a href="#">Disney World Vacations</a></li>
-					<li><a href="#">Vacations As Advertised</a></li>
-				</ul>
-			</div>
-			<!-- End of column 2-->		
-			
-			<div class="col-md-3">
-				<span class="ftitleblack">Travel Specialists</span>
-				<br/><br/>
-				<ul class="footerlistblack">
-					<li><a href="#">Weddings</a></li>
-					<li><a href="#">Accessible Travel</a></li>
-					<li><a href="#">Disney Parks</a></li>
-					<li><a href="#">Cruises</a></li>
-					<li><a href="#">Round the World</a></li>
-					<li><a href="#">First Class Flights</a></li>
-				</ul>				
-			</div>
-			<!-- End of column 3-->		
-			
-			<div class="col-md-3 grey">
-				<span class="ftitleblack">Newsletter</span>
-				<div class="relative">
-					<input type="email" class="form-control fccustom2black" id="exampleInputEmail1" placeholder="Enter email">
-					<button type="submit" class="btn btn-default btncustom">Submit<img src="images/arrow.png" alt=""/></button>
-				</div>
-				<br/><br/>
-				<span class="ftitleblack">Customer support</span><br/>
-				<span class="pnr">1-866-599-6674</span><br/>
-				<span class="grey2">office@travel.com</span>
-			</div>			
-			<!-- End of column 4-->			
-		
-			
-		</div>	
-	</div>
-	
-	<div class="footerbg3black">
-		<div class="container center grey"> 
-		<a href="#">Home</a> | 
-		<a href="#">About</a> | 
-		<a href="#">Last minute</a> | 
-		<a href="#">Early booking</a> | 
-		<a href="#">Special offers</a> | 
-		<a href="#">Blog</a> | 
-		<a href="#">Contact</a>
-		<a href="#top" class="gotop scroll"><img src="images/spacer.png" alt=""/></a>
-		</div>
-	</div>
 	
 	
 	<!-- Javascript  -->
