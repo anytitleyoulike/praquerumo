022ffe3f5a093e287e72461f14d56ccb2a6d3898
Marcello <maarc.hen@gmail.com>
2015-07-17 18:40:20 -0400

    Revert "Revert "setando erros idividuais e criando nova pagina de perfil""
    This reverts commit 2810479009101bc6a605e97ca4d8c651e58fd2b2.

--- application/views/usuarios/cadastro.php
+++ application/views/usuarios/cadastro.php
@@ -3,18 +3,22 @@
 	<head></head>
 	<title>Login PRAQUERUMO</title>
 	<body>
-		<?php echo validation_errors();?>
+		
 		<form action="/praquerumo/usuarios/cadastro" method="post">
 			<h5>Nome:</h5>
+			<?php echo form_error('nome');?>
 			<input type="text" name="nome" value="" placeholder="Nome Completo" size="30">
 			
 			<h5>Telefone:</h5>
+			<?php echo form_error('telefone'); ?>
 			<input type="text" name="telefone" value="" size="30">
 
 			<h5>Email:</h5>
+			<?php echo form_error('email'); ?>
 			<input type="text" name="email" value="" size="30">
 
 			<h5>Senha:</h5>
+			<?php echo form_error('senha'); ?>
 			<input type="text" name="senha" value="" size="30">
 			
 			<div><input type="submit" value="Enviar" /></div>
