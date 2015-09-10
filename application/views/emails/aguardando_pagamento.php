8586dd1743a0c54bafb3bc38390bd3fd1130ddd1
Matheus Azevedo <matheusbzevedo@gmail.com>
2015-09-10 17:27:34 -0400

    dados do usuário adicionados ao e-mail de aguardando pagamento

--- application/views/emails/aguardando_pagamento.php
+++ application/views/emails/aguardando_pagamento.php
@@ -1,142 +1,3 @@
-<!--<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
-	<title>Aguardando Pagamento</title>
-
-</head>
-<body>
-		<table cellspacing="0" cellpadding="0" border="0" width="100%"
-			dir="ltr"
-			style="background-color: #FFF; font-size: 16px">
-			<tbody>
-				<tr>
-					<td align="center" valign="top" style="padding: 0; margin: 0">
-						<table cellspacing="0" cellpadding="0" border="0" align="center"
-							width="597" style="width: 597px; background-color: #FFF;" class="bg-mail">
-							<tbody>
-									<td align="center" valign="top" style="margin: 0; padding: 0">
-										<table cellspacing="0" cellpadding="0" border="0"
-											align="center" width="100%"
-											style="margin-right: 0px; margin-left: 0px">
-											<tbody>
-												<tr>
-													<td align="center" valign="top"
-														style="padding: 40px 50px 0px; margin: 0px; line-height: 1.45; font-family: Helvetica, Trebuchet, Arial, sans-serif; color: rgb(28, 85, 165); background-color: rgb(255, 255, 255)">
-														<table cellspacing="0" cellpadding="0" border="0"
-															align="center" width="98"
-															style="margin-right: 0px; margin-left: 0px">
-															<tbody>
-																<tr>
-																	<td align="right" valign="middle" style="border-top: 8px solid rgb(255, 255, 255); padding: 0px; margin: 0px">
-																		<a href="http:www.praquerumo.com.br"><img width="230" height="87" src="http://www.praquerumo.com.br/assets/images/emails/logo.png"style="border-width: 0px; margin-top: -23; border-style: none; border-color: transparent; display: block"></a>
-																	</td>
-																</tr>
-															</tbody>
-														</table>
-													</td>
-												</tr>
-											</tbody>
-											<tbody>
-												<tr>
-													<td align="center" valign="top"
-														style="padding: 40px 50px 0px; margin: 0px; line-height: 1.45; font-family: Helvetica, Trebuchet, Arial, sans-serif; color: rgb(28, 85, 165); background-color: rgb(255, 255, 255)">
-														<div>
-															<span style="color: rgb(0, 0, 0)"><span
-																style="font-weight: bold"><font
-																	face="Adobe Garamond Pro, serif" size="28"
-																	style="font-size: 28px">Aguardando Pagamento</font></span></span>
-														</div>
-													</td>
-												</tr>
-											</tbody>
-										</table>
-									</td>
-								</tr>
-								<tr>
-									<td align="center" valign="top" style="margin: 0; padding: 0">
-										<table cellspacing="0" cellpadding="0" border="0"
-											align="center" width="100%"
-											style="margin-right: 0px; margin-left: 0px">
-											<tbody>
-												<tr>
-													<td align="justify" valign="top"
-														style="padding: 25px 50px 0px; margin: 0px; line-height: 1.45; font-family: Helvetica, Trebuchet, Arial, sans-serif; color: rgb(75, 75, 75); background-color: rgb(255, 255, 255)">
-														<span
-														style="font-family: Helvetica, Trebuchet, Arial, sans-serif; color: #4b4b4b; font-size: 18px; font-weight: normal"></span>
-														<p>Olá <?=$nome?>,</p>
-														<div>
-															<p> Tomamos conhecimento de seu pedido por <b>BOLETO BANCÁRIO</b> e estamos felizes pelo seu interesse :) </p>
-															<p>
-																Você deve ter recebido sua fatura do <b>BOLETO</b> quando foi redirecionado através do nosso site. Por
-gentileza, imprima a página referente ao boleto e leve-a com você para realizar o depósito. Você
-pode efetuar o depósito na maioria dos bancos ou através do site de seu banco.
-															</p>
-															<p>É importante lembra-lo de que a fatura do <b>BOLETO</b> vale somente por 24 horas!</p>
-															<p>Seu boleto pode ser visualizado facilmente aqui: <h3><a href="<?=$url?>">Clique aqui para visualizar sua fatura</a></h3></p>
-
-															<p>Depois de realizar o depósito, pode levar até 1 dia útil para recebermos o seu depósito. Depois disso, enviaremos seu voucher para esse email.</p>
-															<p>Caso tenha quaisquer perguntas, por gentileza não hesite em nos contactar pelo email
-<b>contato@praquerumo.com.br.</b></p>
-															<p>Atenciosamente,</p>
-
-															<p>Equipe Pra Que Rumo.</p>
-
-														</div>
-													</td>
-												</tr>
-											</tbody>
-										</table>
-									</td>
-								</tr>
-								<tr>
-									<td><table cellspacing="0" cellpadding="0" align="center"
-											style="margin-right: auto; margin-left: auto">
-											<tbody>
-												<tr>
-													<td align="left" valign="top"
-														style="padding: 10px; margin: 0px"><table
-															cellspacing="0" cellpadding="0" align="center">
-															<tbody>
-																<tr>
-																	<td style="display: inline-block !important; width: auto !important; margin: auto; padding: 5px">
-																		<a title="Facebook" target="_blank" href="https://www.facebook.com/praquerumo"><img border="0" style="display: block" alt="Like" src="http://www.praquerumo.com.br/assets/images/icone_facebook.png"></a>
-																	</td>
-																	<td style="display: inline-block !important; width: auto !important; margin: auto; padding: 5px">
-																		<a class="logo-pqr" title="PRAQUERUMO" target="_blank" href="http://www.praquerumo.com.br"><img border="0" style="display: block" alt="PRAQUERUMO" src="http://www.praquerumo.com.br/assets/images/icone_pqr.png"></a>
-																	</td>
-																	<td style="display: inline-block !important; width: auto !important; margin: auto; padding: 5px">
-																		<a title="Twitter" target="_blank" href="https://twitter.com/PraQueRumo"><img border="0" style="display: block" alt="Twitter" src="http://www.praquerumo.com.br/assets/images/icone_twitter.png"></a>
-																	</td>
-																</tr>
-															</tbody>
-														</table></td>
-												</tr>
-											</tbody>
-											<tbody></tbody>
-										</table></td>
-								</tr>
-								<tr>
-									<td></td>
-								</tr>
-								<tr>
-									<td></td>
-								</tr>
-
-
-							</tbody>
-						</table>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-
-</body>
-</html>-->
-
-<!-- TESTE EMAIL NOVO -->
-
 <!DOCTYPE html>
 <html>
 	<head>
@@ -227,9 +88,23 @@ pode efetuar o depósito na maioria dos bancos ou através do site de seu banco.
 							</tr>
 						</td>
 					</tr>
-					<tr height='42px'>
-						<td height='42px'>
-							&nbsp;
+					<tr>
+						<td>
+							<table width='100%' border='0'>
+								<tr>
+									<td colspan='2' align='center'><font style='margin-top:12px; color:#808080;'>DADOS DO CLIENTE</font></td>
+								</tr>
+								<tr>
+									<td width='50%'><font style='margin-left:37px; color:#808080;'><strong>Nome</strong>: <?=$usuario['nome']?></font></td>
+									<td align='center'><font style='color:#808080;'><strong>Tel</strong>: <?=$usuario['telefone']?></font></td>
+								</tr>
+								<tr>
+									<td colspan='2' width='50%'><font style='margin-left:37px; color:#808080;'><strong>E-mail</strong>: <?=$usuario['email']?></font></td>
+								</tr>
+								<tr>
+									<td colspan='2'><hr/></td>
+								</tr>
+							</table>
 						</td>
 					</tr>
 				</table>
